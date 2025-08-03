87d7d261c3f94b8fafe865f4d21d0af558338403
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2025-08-03 14:33:12 +0200

    Fix clippy issue

--- zellij/src/main.rs
+++ zellij/src/main.rs
@@ -99,8 +99,7 @@ impl ZellijPlugin for State {
             }
             Event::PaneClosed(pane_id_enum) => {
                 let pane_id = match pane_id_enum {
-                    PaneId::Terminal(pane_id) => pane_id,
-                    PaneId::Plugin(pane_id) => pane_id,
+                    PaneId::Terminal(pane_id) | PaneId::Plugin(pane_id) => pane_id,
                 };
 
                 self.panes.panes = self
